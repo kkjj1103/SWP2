04a54532f872c6f0e6ba4a5e4bd4260cf7875243
kmu-gyumin <155567361+kkjj1103@users.noreply.github.com>
2024-09-30 00:00:42 +0900

    Update 05_practice_2.ino

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -1,23 +1,22 @@
 #define PIN_LED 7
 
 void setup() {
-  // LED 핀을 출력모드로 설정
   pinMode(PIN_LED, OUTPUT);
 }
 
 void loop() {
 
-  digitalWrite(PIN_LED, 1);
+  digitalWrite(PIN_LED, 0);
   delay(1000);
   
   for(int i = 0; i < 5; i++){
-    digitalWrite(PIN_LED, 0); //LED를 끔
+    digitalWrite(PIN_LED, 1);
     delay(100);
-    digitalWrite(PIN_LED, 1); //LED를 킴
+    digitalWrite(PIN_LED, 0);
     delay(100);
   }
 
-  digitalWrite(PIN_LED, 0);
+  digitalWrite(PIN_LED, 1);
 
   while(1){
     // infinite loop
